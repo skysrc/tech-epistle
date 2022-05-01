abf84077db9df04de37de11e9c45a38c4e0e189e
Sky <skymsdn@hotmail.com>
2022-05-01 22:39:04 +0800

    update site name removed unused navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,36 +1,36 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://skysrc.github.io',
-  baseUrl: '/tech-epistle/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'skysrc', // Usually your GitHub org/user name.
-  projectName: 'tech-epistle', // Usually your repo name.
+  title: "Tech-Epistle",
+  tagline: "Dinosaurs are cool",
+  url: "https://skysrc.github.io",
+  baseUrl: "/tech-epistle/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "skysrc", // Usually your GitHub org/user name.
+  projectName: "tech-epistle", // Usually your repo name.
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: false, // Optional: disable the docs pluginfalse, // Optional: disable the docs plugin
         blog: {
-          routeBasePath: '/', // Serve the blog at the site's root
+          routeBasePath: "/", // Serve the blog at the site's root
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -40,14 +40,14 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Tech-Epistle',
+        title: "Tech-Epistle",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        }
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         copyright: `Copyright © ${new Date().getFullYear()} Tech-Epistle, Inc. Built with Docusaurus.`,
       },
       prism: {
